848a416ea287774e7a5858c9931486bed94abd8d
CaoTianze <hnrcao@qq.com>
2024-06-10 16:25:40 +0800

    base geom create

--- R/cell-geom.R
+++ R/cell-geom.R
@@ -16,3 +16,62 @@ ccText = function(...) {
   new("ccCellGeom",func = 'circos.text',params = list(...))
 }
 
+#' @export
+ccPoints = function(...) {
+  new("ccCellGeom",func = 'circos.points',params = list(...))
+}
+
+#' @export
+ccLines = function(...) {
+  new("ccCellGeom",func = 'circos.lines',params = list(...))
+}
+
+#' @export
+ccSegments = function(...) {
+  new("ccCellGeom",func = 'circos.segments',params = list(...))
+}
+
+#' @export
+ccRect = function(...) {
+  new("ccCellGeom",func = 'circos.rect',params = list(...))
+}
+
+#' @export
+ccPolygon = function(...) {
+  new("ccCellGeom",func = 'circos.polygon',params = list(...))
+}
+
+#' @export
+ccXaxis = function(...) {
+  new("ccCellGeom",func = 'circos.xaxis',params = list(...))
+}
+
+#' @export
+ccYaxis = function(...) {
+  new("ccCellGeom",func = 'circos.yaxis',params = list(...))
+}
+
+#' @export
+ccBarplo = function(...) {
+  new("ccCellGeom",func = 'circos.barplot',params = list(...))
+}
+
+#' @export
+ccBoxplot = function(...) {
+  new("ccCellGeom",func = 'circos.boxplot',params = list(...))
+}
+
+#' @export
+ccViolin = function(...) {
+  new("ccCellGeom",func = 'circos.violin',params = list(...))
+}
+
+#' @export
+ccArrow = function(...) {
+  new("ccCellGeom",func = 'circos.arrow',params = list(...))
+}
+
+#' @export
+ccRaster = function(...) {
+  new("ccCellGeom",func = 'circos.raster',params = list(...))
+}
--- R/track-geom.R
+++ R/track-geom.R
@@ -1,8 +1,22 @@
 #' @export
 setClass("ccTrackGeom",
          slots = c(func = "character", params = "list"))
+#' @export
+ccTrackHist = function(...) {
+  new("ccTrackGeom",func = 'circos.trackHist', params = list(...))
+}
+
+#' @export
+ccTrackLines = function(...) {
+  new("ccTrackGeom",func = 'circos.trackLines', params = list(...))
+}
 
 #' @export
 ccTrackPoints = function(...) {
   new("ccTrackGeom",func = 'circos.trackPoints', params = list(...))
+}
+
+#' @export
+ccTrackText = function(...) {
+  new("ccTrackGeom",func = 'circos.trackText', params = list(...))
 }
