4671f4ebda92b2d93d9703e9e8a87b97f27855cf
CaoTianze <hnrcao@qq.com>
2025-04-14 10:06:33 +0800

    data

---
+++ R/data.R
@@ -0,0 +1,49 @@
+#' Allele Frequency
+#'
+#' A DataFrame is used for the parameter data in ccGenomicPoints(). Example can be found in "4.2 Example 2: comparison of two pieces of code that use circlizePlus and circlize to implement the same requirements, respectively"
+#'
+#' @format A dataframe with 25,000 rows and 4 columns
+#' @usage data("example2")
+#' @source \url{https://www.frontiersin.org/journals/genetics/articles/10.3389/fgene.2025.1535368/full#h5}
+#'
+"allele_frequency"
+
+#' Copy Number
+#'
+#' A DataFrame is used for the parameter data in ccGenomicPoints(). Example can be found in "4.2 Example 2: comparison of two pieces of code that use circlizePlus and circlize to implement the same requirements, respectively"
+#'
+#' @format A dataframe with 24,788 rows and 5 columns
+#' @usage data("example2")
+#' @source \url{https://www.frontiersin.org/journals/genetics/articles/10.3389/fgene.2025.1535368/full#h5}
+#'
+"copy_number"
+
+#' Junctions
+#'
+#' A DataFrame is used for the parameter data in ccGenomicLink(). Example can be found in "4.2 Example 2: comparison of two pieces of code that use circlizePlus and circlize to implement the same requirements, respectively"
+#'
+#' @format A dataframe with 2414 rows and 28 columns
+#' @usage data("example2")
+#' @source \url{https://www.frontiersin.org/journals/genetics/articles/10.3389/fgene.2025.1535368/full#h5}
+#'
+"junctions"
+
+#' R1
+#'
+#' A DataFrame is used for the parameter data in ccGenomicLink(). Example can be found in "4.2 Example 2: comparison of two pieces of code that use circlizePlus and circlize to implement the same requirements, respectively"
+#'
+#' @format A dataframe with 2414 rows and 3 columns
+#' @usage data("example2")
+#' @source \url{https://www.frontiersin.org/journals/genetics/articles/10.3389/fgene.2025.1535368/full#h5}
+#'
+"r1"
+
+#' R2
+#'
+#' A DataFrame is used for the parameter data in ccGenomicLink(). Example can be found in "4.2 Example 2: comparison of two pieces of code that use circlizePlus and circlize to implement the same requirements, respectively"
+#'
+#' @format A dataframe with 2414 rows and 3 columns
+#' @usage data("example2")
+#' @source \url{https://www.frontiersin.org/journals/genetics/articles/10.3389/fgene.2025.1535368/full#h5}
+#'
+"r2"
