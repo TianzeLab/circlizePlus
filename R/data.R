7a24169a50f0418a65a66687861ab3b344b486b0
CaoTianze <hnrcao@qq.com>
2025-04-15 15:32:12 +0800

    modify data set

--- R/data.R
+++ R/data.R
@@ -1,51 +1,14 @@
 #' Data set in circlizePlus
 #'
 #' Example can be found in "4.2 Example 2: comparison of two pieces of code that use circlizePlus and circlize to implement the same requirements, respectively"
-#'
-#' @usage data("example2")
+#' <br><br>allele_frequency: A dataframe with 25,000 rows and 4 columns
+#' <br>copy_number: A dataframe with 24,788 rows and 5 columns
+#' <br>junctions: A dataframe with 2414 rows and 28 columns
+#' <br>r1: A dataframe with 2414 rows and 3 columns
+#' <br>r2: A dataframe with 2414 rows and 3 columns
 #' @source \url{https://www.frontiersin.org/journals/genetics/articles/10.3389/fgene.2025.1535368/full#h5}
 #' @examples
-#' NULL
+#' data("example2")
 #'
 #' @name data-set
 NULL
-
-#'
-#' @description allele_frequency: A DataFrame is used for the parameter data in ccGenomicPoints().
-#'
-#' @rdname data-set
-#' @format allele_frequency: A dataframe with 25,000 rows and 4 columns
-#'
-"allele_frequency"
-
-#'
-#' @description copy_number: A DataFrame is used for the parameter data in ccGenomicPoints().
-#'
-#' @rdname data-set
-#' @format copy_number: A dataframe with 24,788 rows and 5 columns
-#'
-"copy_number"
-
-#'
-#' Junctions: A DataFrame is used for the parameter data in ccGenomicLink().
-#'
-#' @rdname data-set
-#' @format junctions: A dataframe with 2414 rows and 28 columns
-#'
-"junctions"
-
-#'
-#' @description r1: A DataFrame is used for the parameter data in ccGenomicLink().
-#'
-#' @rdname data-set
-#' @format r1: A dataframe with 2414 rows and 3 columns
-#'
-"r1"
-
-#'
-#' @description r2: A DataFrame is used for the parameter data in ccGenomicLink().
-#'
-#' @rdname data-set
-#' @format r2: A dataframe with 2414 rows and 3 columns
-#'
-"r2"
