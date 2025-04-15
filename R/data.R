3a5ffc827e603db1321b271b05755a7d25fd0660
CaoTianze <hnrcao@qq.com>
2025-04-15 16:57:54 +0800

    update Rd

--- R/data.R
+++ R/data.R
@@ -9,12 +9,14 @@
 #' @name data-set
 #' @source \url{https://www.frontiersin.org/journals/genetics/articles/10.3389/fgene.2025.1535368/full#h5}
 #' @examples
+#' \dontrun{
 #' data(example2)
 #' allele_frequency
 #' copy_number
 #' junctions
 #' r1
 #' r2
+#' }
 NULL
 
 #' @rdname data-set
